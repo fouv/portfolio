0b23b69ded467d52d264bcc6f6ac7fbbc2fee0bb
Florence Bouvier <florencebres1@gmail.com>
2019-05-08 17:10:52 +0200

    base implementation

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,8 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class HomeController extends AbstractController
 {
@@ -13,5 +14,18 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('default/index.html.twig');
+    }
+}
+
+class PdfController extends AbstractController
+{
+  /**
+   * @Route("/cv_reactualise", name="cv")
+   */
+    public function downloadAction()
+    {
+        $pdfPath = $this->getParameter('dir.downloads').'doc/cvReactualise210419.pdf';
+
+        return $this->file($pdfPath, 'sample.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
     }
 }
